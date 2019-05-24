e293bfd3c474343f7a7777a322da67aa0b673206
anderson <jkr.dev@hotmail.com>
2019-05-23 21:47:17 -0300

    inital

---
+++ src/DSUser.Api/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DSUser.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static ICollection<string> users;
+
+        public UserController()
+        {
+            users = users ?? new List<string>();
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(users.OrderBy(o => o));
+        }
+
+        // GET api/values/5
+        [HttpGet("{name}")]
+        public IActionResult Get([FromRoute] string name)
+        {
+            return Ok(users.FirstOrDefault(u => u.Equals(name)));
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] string name)
+        {
+            users.Add(name);
+            return Ok();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete]
+        public IActionResult Delete([FromBody] string name)
+        {
+            users.Remove(name);
+            return Ok();
+        }
+    }
+}
---
+++ src/DSUser.Api/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace DSUser.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.Converters.Add(new StringEnumConverter());
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
